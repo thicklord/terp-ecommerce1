7434d13c20b50505d581473096acecfb91ae0683
C. Sanchez <7hickl0rd@gmail.com>
2023-12-10 16:36:40 -0500

    Finishing header and nav section.

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -8,6 +8,8 @@ import { useAuth } from '../../../_providers/Auth'
 import { CartLink } from '../../CartLink'
 import { CMSLink } from '../../Link'
 
+import { Button } from '../../Button'
+
 import classes from './index.module.scss'
 
 export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
@@ -31,11 +33,15 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
       <CartLink />
       {user && <Link href="/account">Account</Link>}
       {!user && (
-        <React.Fragment>
-          <Link href="/login">Login</Link>
-          <Link href="/create-account">Create Account</Link>
-        </React.Fragment>
+        <Button
+          el="link"
+          href="/login"
+          label="Login"
+          appearance="primary"
+          onClick={() => (window.location.href = '/login')}
+        />
       )}
+      {user && <CartLink />}
     </nav>
   )
 }
